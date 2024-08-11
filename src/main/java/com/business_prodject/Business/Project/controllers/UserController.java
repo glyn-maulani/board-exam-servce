522b9b951f8fd833665b62412f58d0f1f4952817
Imman_Ella <christian.ella@awsys-i.com>
2024-08-11 14:43:23 +0800

    add CrossOrigin to userController for react data fetch

--- src/main/java/com/business_prodject/Business/Project/controllers/UserController.java
+++ src/main/java/com/business_prodject/Business/Project/controllers/UserController.java
@@ -4,6 +4,7 @@ package com.business_prodject.Business.Project.controllers;
 import com.business_prodject.Business.Project.entities.user;
 import com.business_prodject.Business.Project.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     private final UserService userService;
 
